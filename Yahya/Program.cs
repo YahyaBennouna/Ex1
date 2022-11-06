22429cbe1471edf5862c25f3795f9c72855a58f9
benn <benn@DESKTOP-IMTHR3V>
2022-11-06 17:40:15 +0100

    Ajoutez des fichiers projet.

---
+++ Yahya/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+class Yahya
+{
+    public static void Main(string[] args)
+    {
+        int r;
+        bool co = false;
+
+        Console.WriteLine("Entrer le nombre ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        for (int i = 2; i < n; i++)
+        {
+            r = n % i;
+            i++;
+            if (r == 0)
+            {
+                co = true;
+            }
+        }
+        if (co == true)
+
+            Console.WriteLine("votre nombre n est pas premier ");
+        else
+        {
+            Console.WriteLine("votre nombre est  premier ");
+        }
+    }
+}
